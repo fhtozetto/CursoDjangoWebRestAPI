d11e6a0aea63a265ebb6edff8d5aff1ea059fe5e
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2025-05-05 14:27:03 -0300

    Uma nova URL para uma nova View - sobre URLs no Django

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otávio',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
